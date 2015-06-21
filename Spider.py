0c40360e7c55e2df8b9e7bcc2f83188c6b0a5a1b
xueweihan <595666367@qq.com>
2015-06-21 20:24:03 +0800

    v0.8 有Bug

--- Spider.py
+++ Spider.py
@@ -89,32 +89,62 @@ def main_content(html):
 #<h2><a href="http://bohaishibei.com/post/10262/" title="[博海拾贝0609期]今天是个好日子 - 博海拾贝">[博海拾贝0609期]今天是个好日子</a></h2>  
 
 #进page_url：http://bohaishibei.com/post/10475/" title="[博海拾贝0620期]今天没吃粽子的不止我一个人吧 |
-def page_url(content, beg = 0):
+
+# 新增一个参数order，默认为20
+def page_url(content, order = 20, beg = 0):
     try:
         url = []
-        while True:
+        i = 0
+        while i < order:
             url1 = content.index('<h2><a href="',beg)+13
             url2 = content.index('" ',url1)
             url.append(content[url1:url2])
             beg = url2
+            i = i + 1
+        return url
     except ValueError:
         return url   
         
 def get_order(num):
-    page = num / 20
+# num代表获取的条目数量
+    url_list = []
+    page = num / 20 
     order = num % 20 # 超出一整页的条目
+    if num < 20:  # 如果获取的条目数量少于20（一页20个），直接爬取第一页的num条
+        url = 'http://bohaishibei.com/post/category/main'
+        main_html = getHtml(url)
+        clean_content = main_content(main_html)         
+        url_list = url_list + page_url(clean_content, num)  
     for i in range(1, page+1): # 需这里需要尾巴
-        url = 'http://bohaishibei.com/post/category/main/page/%d' % i 
-        
-    if i == pageorder > 0:
-           
-        print url
-    
-    
-    print page
-    print '~~~~~~~~~~~~'
-    print order
-get_order(55)        
+        url = 'http://bohaishibei.com/post/category/main/page/%d' % i # 爬取整页的条目
+        main_html = getHtml(url)
+        clean_content = main_content(main_html)
+        url_list = url_list + page_url(clean_content) #获取整夜
+      
+        if (i == page)&(order > 0):  # 爬到最后一页，如果有超出一页的条目则继续怕order条
+            url = 'http://bohaishibei.com/post/category/main/page/%d' % (i+1) 
+            main_html = getHtml(url)
+            clean_content = main_content(main_html)         
+            url_list = url_list + page_url(clean_content, order)            
+            #print len(page_url(clean_content, order))
+    return url_list
+
+#html = getHtml("http://bohaishibei.com/post/10475/")             
+#content = content(html)
+#title = title(content)
+#img = get_img(content)
+#data_out(title, img)
+# 实现了爬的单个页面的title和img的url并存入文本      
+order = get_order(21)
+for i in range(0, len(order)):
+    html = getHtml(order[i]) 
+    #print html
+    #print type(html)            
+    content_data = content(html)
+    title_data = title(content_data)
+    img_data = get_img(content_data)
+    data_out(title_data, img_data)
+#print len(get_order(21))
 #main_html = getHtml("http://bohaishibei.com/post/category/main/page/6/")
 #clean_content = main_content(main_html) 
 #str = page_url(clean_content)
